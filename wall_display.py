0894f0d3fbf4f678e1f9269c7a334074acd79e71
Morten Jagd Christensen <morten.christensen@esss.se>
2017-06-16 11:22:55 +0200

    working on improvements

--- wall_display.py
+++ wall_display.py
@@ -11,49 +11,49 @@ import getpass      # getpass.getpass
 
 class wallDisplay(object):
     def __init__(self):
-        self.drivers = []
-        self.groups = []
+        self.groups = {}
 
     def login(self, driver, nuser, npass, tuser, tpass):
-       try:
-          username = driver.find_element_by_name(nuser)
-          password = driver.find_element_by_name(npass)
-       except Exception:
-          print("find_element_by_name failed")
-          return
+        try:
+            username = driver.find_element_by_name(nuser)
+            password = driver.find_element_by_name(npass)
+        except Exception:
+            print("find_element_by_name failed")
+            return
 
-       username.send_keys(Keys.CLEAR)
-       username.send_keys(tuser)
-
-       password.send_keys(Keys.CLEAR)
-       password.send_keys(tpass)
-       password.send_keys(Keys.RETURN)
+        username.send_keys(Keys.CLEAR)
+        username.send_keys(tuser)
+        password.send_keys(Keys.CLEAR)
+        password.send_keys(tpass)
+        password.send_keys(Keys.RETURN)
 
     def register(self, group, url, refreshNeeded, nusr, tusr, npwd, tpwd):
-        pos = 0
-        for g in self.groups:
-            if group == g[0]:
-                pos = pos + 1
-        self.groups.append([group, pos, url, refreshNeeded, nusr, tusr, npwd, tpwd])
+        if not group in self.groups:
+            self.groups[group] = []
+        self.groups[group].append([url, refreshNeeded, nusr, tusr, npwd, tpwd])
 
     # @todo currently only works for a single group and up to four windows
-    def pos2xy(self, pos):
+    def pos2xy(self, pos, n):
+        print("pos %d, n %d, x %d\n" % (pos, n, pos%(n/2)))
+
         if pos == 0:
-            return [2000, 0]
+            return [2000,    0, 2000, 1000]
         elif pos == 1:
-            return [4000, 0]
+            return [4000,    0, 2000, 1000]
         elif pos == 2:
-            return [2000, 1050]
+            return [2000, 1075, 2000, 1000]
         elif pos == 3:
-            return [4000, 1050]
+            return [4000, 1075, 2000, 1000]
 
-    def launch2(self):
-        for g, pos, url, ref, nu, tu, np, tp in self.groups:
-            x,y = self.pos2xy(pos)
-            self.launch(url, x, y, ref, nu, tu, np, tp)
+    def launch(self):
+        for g in self.groups:
+            cGroup = self.groups[g]
+            for pos, view in enumerate(cGroup):
+                x, y, w, h = self.pos2xy(pos, len(cGroup))
+                drv = self.weblaunch(x, y, w, h, *view)
+                view.append(drv)
 
-
-    def launch(self, url, x, y, refreshNeeded, nusr, tusr, npwd, tpwd):
+    def weblaunch(self, x, y, w, h, url, refreshNeeded, nusr, tusr, npwd, tpwd):
        profile = webdriver.FirefoxProfile()
        ext_path = "ff_ext/"
        profile.add_extension(extension=ext_path + "hide_tabbar-2.1.0-fx.xpi")
@@ -65,30 +65,27 @@ class wallDisplay(object):
 
        driver = webdriver.Firefox(profile, timeout = 100)
        driver.set_window_position(x, y)
-       driver.set_window_size(2000, 1000)
+       driver.set_window_size(w, h)
        try:
           driver.get(url)
        except Exception:
           driver.close()
 
-       self.drivers.append([driver, url, refreshNeeded])
        if nusr != "":
           self.login(driver, nusr, npwd, tusr, tpwd)
-
+       return driver
 
     def updateloop(self, updint):
         try:
-           while True:
-              time.sleep(updint)
-              for x in self.drivers:
-                 drv = x[0]
-                 url = x[1]
-                 ref = x[2]
-                 if ref:
-                    drv.get(url)
+            while True:
+                time.sleep(updint)
+                for g in self.groups:
+                    cGroup = self.groups[g]
+                    for url, refreshNeeded, nusr, tusr, npwd, tpwd, drv in cGroup:
+                        if refreshNeeded:
+                            drv.get(url)
         except Exception:
             self.exit()
-
 
     def exit(self):
         for x in self.drivers:
@@ -114,9 +111,10 @@ def main():
     walldisp.register(0, "http://jenkins.esss.dk/dm",  1, "", "", "", "")
     walldisp.register(0, "http://127.0.0.1:3000",      0, "username", "admin", "password", "admin")
     walldisp.register(0, "https://jira.esss.lu.se/secure/RapidBoard.jspa?rapidView=167&projectKey=DM&view=reporting&chart=cumulativeFlowDiagram&swimlane=287&swimlane=288&column=674&column=734&column=675&column=678&column=677&column=676" , 1, "UserName", user, "Password", paswd)
+    #walldisp.register(1, "http://status.esss.se",      1, "", "", "", "")
 
-    walldisp.launch2()
-    walldisp.updateloop(180)
+    walldisp.launch()
+    #walldisp.updateloop(3)
 
 if __name__ == "__main__":
     main()

db7b96f9b85a871a3f33a81231cdb8ad719b06f8
Morten Jagd Christensen <morten.christensen@esss.se>
2017-06-19 13:34:41 +0200

    sort of working ok for a first beta version

--- wall_display.py
+++ wall_display.py
@@ -19,8 +19,10 @@ class wallDisplay(object):
 
     def pos2xy(self, n, max):
         if max < 1 or max > 8:
+            print("too many windows")
             return ""
         if n > max or n < 1:
+            print("current window out of range")
             return ""
 
         w = self.w
@@ -45,7 +47,7 @@ class wallDisplay(object):
             w = w/4
             h = h/2
             d = 4
-        return [xo + ((n-1)%d)*w, yo + ((n-1)/d)*h, w, h]
+        return [xo + ((n - 1) % d) * w, yo + ((n - 1) / d) * h, w - 25 , h - 75]
 
     def login(self, driver, nuser, npass, tuser, tpass):
         try:
@@ -71,7 +73,8 @@ class wallDisplay(object):
         for g in self.groups:
             cGroup = self.groups[g]
             for pos, view in enumerate(cGroup):
-                x, y, w, h = self.pos2xy(pos, len(cGroup))
+                x, y, w, h = self.pos2xy(pos + 1, len(cGroup))
+                print("x %d, y %d, w %d, h %d" % (x,y,w,h))
                 drv = self.weblaunch(x, y, w, h, *view)
                 view.append(drv)
 
@@ -105,6 +108,7 @@ class wallDisplay(object):
                     cGroup = self.groups[g]
                     for url, refreshNeeded, nusr, tusr, npwd, tpwd, drv in cGroup:
                         if refreshNeeded:
+
                             drv.get(url)
         except Exception:
             self.exit()
@@ -120,7 +124,7 @@ def main():
        print("usage: wall_display.py username")
        sys.exit(0)
 
-    walldisp = wallDisplay(2000, 0, 4000, 2150)
+    walldisp = wallDisplay(1990, 0, 4055, 2180)
 
     user=sys.argv[1]
     print("please enter password for user " + user)
@@ -129,14 +133,17 @@ def main():
     #os.environ["DISPLAY"] = ":10.0"
     os.environ["DISPLAY"] = ":0.0"
 
-    walldisp.register(0, "http://status.esss.se",      1, "", "", "", "")
+    walldisp.register(0, "https://jenkins.esss.dk/dm/view/Monitor%20view/",      1, "", "", "", "")
+    walldisp.register(0, "https://jenkins.esss.dk/dm/view/ess-dmsc%20master/",      1, "", "", "", "")
     walldisp.register(0, "http://jenkins.esss.dk/dm",  1, "", "", "", "")
+    walldisp.register(0, "https://github.com/orgs/ess-dmsc/dashboard",  1, "", "", "", "")
+    walldisp.register(0, "http://status.esss.se",      1, "", "", "", "")
     walldisp.register(0, "http://127.0.0.1:3000",      0, "username", "admin", "password", "admin")
-    walldisp.register(0, "https://jira.esss.lu.se/secure/RapidBoard.jspa?rapidView=167&projectKey=DM&view=reporting&chart=cumulativeFlowDiagram&swimlane=287&swimlane=288&column=674&column=734&column=675&column=678&column=677&column=676" , 1, "UserName", user, "Password", paswd)
-    #walldisp.register(1, "http://status.esss.se",      1, "", "", "", "")
+    #walldisp.register(0, "https://jira.esss.lu.se/secure/RapidBoard.jspa?rapidView=167&projectKey=DM&view=reporting&chart=cumulativeFlowDiagram&swimlane=287&swimlane=288&column=674&column=734&column=675&column=678&column=677&column=676" , 1, "UserName", user, "Password", paswd)
 
     walldisp.launch()
-    #walldisp.updateloop(3)
+    print("Entering main loop")
+    walldisp.updateloop(30)
 
 if __name__ == "__main__":
     main()
